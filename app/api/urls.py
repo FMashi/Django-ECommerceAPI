2b30bb9175d7f3f2ab37880ce917798a8047fddf
Maryam Alnassar <122679367+Maryam114@users.noreply.github.com>
2023-12-15 15:53:14 +0300

    update urls files

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,re_path,include
 from  .views import Customers_list
 
 urlpatterns = [
-path('Customers_list/',Customers_list,name='Customers_list')
+  path('Customers_list/',Customers_list,name='Customers_list'),
 ]
